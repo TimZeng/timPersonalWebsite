512275e9bd71914b2400e4a15683ed54d4ce7e8b
Tim Zeng <zengtiantian1122@gmail.com>
2017-04-27 18:02:42 -0700

    add console log to debug heroku

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+console.log('in home component, about to log header');
+
 import Header from './header';
 import About from './about';
+
+console.log('loaded header, trying to render');
+
 
 const Home = () => (
   <div>
