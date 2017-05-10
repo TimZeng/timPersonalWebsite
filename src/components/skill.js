6a2368f47b87ec99916ccf61a11493b8f5df8cc7
Tim Zeng <zengtiantian1122@gmail.com>
2017-05-10 12:12:34 -0700

    create technical skill section

---
+++ src/components/skill.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import $ from 'jquery';
+import Waypoint from 'react-waypoint';
+
+class Skill extends Component {
+  render() {
+    return (
+      <section className="section-skill js--section--skill" id="skill">
+
+        <div className="row">
+          <h2>Technical Skills</h2>
+
+          <p className="long-copy">
+            I have worked with
+          </p>
+
+
+        </div>
+
+      </section>
+    );
+  }
+}
+
+export default Skill;
